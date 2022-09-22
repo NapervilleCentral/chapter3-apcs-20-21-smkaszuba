cadb3592947590bff842c32580dbdb6b2094c554
smkaszuba <smkaszuba2@stu.naperville203.org>
2022-09-22 14:19:14 -0500

    b

---
+++ chapter 3 if programs/gradeConvert.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class gradeConvert
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String letterGrade;
+        float numGrade;
+        
+        System.out.print("Enter a letter grade: ");
+        letterGrade = in.nextLine();
+        
+        switch (letterGrade.substring(0)){
+            case 
+    }
+}
