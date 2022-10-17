a883d06d1bdadbd55d88d6b85286dc6c41c9abb1
Steven M Kaszuba2 <SMKaszuba2@student.naperville203.org>
2022-10-17 14:14:42 -0500

    d

---
+++ chapter3b/monteyGuy.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class monteyGuy{
+    public static void main(String[] args){
+        int pick, door, wrongDoor, win = 0, loss = 0;
+        Random random = new Random();
+        for(int x = 0; x < 1000; x++){
+            door = random.nextInt(3) + 1;
+            pick = random.nextInt(3) + 1;
+            if (door == pick)
+                loss++;
+            else{
+                win++;
+            }
+        }
+        System.out.print("Switch Wins: " + win + "\nLosses: " + loss);
+        win = 0;
+        loss = 0;
+        for (int x = 0; x < 1000; x++){
+            door = random.nextInt(3) + 1;
+            pick = random.nextInt(3) + 1;
+            if (door == pick)
+                win++;
+            else
+                loss++;
+        }
+        System.out.print("\nNo switch Wins: " + win + "\nLosses: " + loss);
+        }
+    }
