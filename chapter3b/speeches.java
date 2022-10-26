c728735f3743fe62cf31d8b050186106543b4c6c
smkaszuba <smkaszuba2@stu.naperville203.org>
2022-10-26 13:55:00 -0500

    asdf

--- chapter3b/speeches.java
+++ chapter3b/speeches.java
@@ -1,28 +1,47 @@
+// Mitchell Kaszuba
+// 10/25/22
+// speeches program
+// analyses speeches
 import java.util.*;
 import java.io.*;
 
 class speeches{
   public static void main(String[] args) throws IOException{
-    File file = new File("speeches/jfk.txt");
+    // file is the file being analized
+    File file = new File("speeches/Abraham Lincoln A House Divided.txt");
     Scanner in = new Scanner(file);
+    
+    // words - amount of words in speech
+    // total - total length of all words in speech
+    // average - average between the length of every word and amount of words
     int words = 0, average, total = 0, length;
     boolean united = false, government = false, people = false, our = false;
     String word;
     
+    // while file has letters in it
     while (in.hasNext()){
+      // word is next word in file
       word = in.next();
+      // add length of word to total length of all words
       total += word.length();
-      if (word == "united")
+      
+      // if the given word is one of these words, declare that the word was said
+      if (word.toLowerCase().contains("united"))
         united = true;
-      if (word == "government")
+      if (word.toLowerCase().contains("government"))
         government = true;
-      if (word == "people")
-        united = true;
-      if (word == "our")
+      if (word.toLowerCase().contains("people"))
+        people = true;
+      if (word.toLowerCase().contains("our"))
         our = true;
+      // increase amount of words said by 1
       words++;
     }
+    
+    // calculate average
     average = total / words;
+    
+    // display info to user
     System.out.println("file: " + file);
     System.out.println("# of words: " + words);
     System.out.println("average length of words: " + average);
--- chapter3b/star.java
+++ chapter3b/star.java
@@ -1,14 +1,19 @@
-public class star
-{
-    
+// Mitchell Kaszuba
+// 10/22/22
+// star
+// makes a triangle of stars
+public class star{ 
    public static void main(String[] args){
-       String str = "**********";
-       
- 
+       // for loop is each row
        for (int n = 0; n < 10; n++){
-           for (int spaces = 10; spaces < 0; spaces--)
+           // this for loop prints spaces
+           // for every row it increases the amount of spaces printed
+           for (int spaces = 0; spaces < n; spaces++)
                System.out.print(" ");
-           for (int stars = 0; stars < 10; stars++)
+           
+           // This for loop prints stars
+           // for every row it decreases the amount of stars printed
+           for (int stars = 10; stars > n; stars--)
                System.out.print("*");
            System.out.println();
        }
