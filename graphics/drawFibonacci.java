8d468ec14b634f228d6436ec49b7b8cd74fb4519
smkaszuba <smkaszuba2@stu.naperville203.org>
2022-11-01 13:45:27 -0500

    asdf

--- graphics/drawFibonacci.java
+++ graphics/drawFibonacci.java
@@ -12,14 +12,25 @@ public class drawFibonacci{
         int term1 = 0, term2 = 1, nextTerm = 1;
         pen.up();
         
-        // for 15 iterations:
-        for (int n = 0; n < 15; n++){
+        for (int n = 0; n < 11; n++){
+            for (int i = 0; i < 4; i++){
+                pen.move(nextTerm);
+                pen.turn(90);
+            }
+            pen.turn(90);
+            pen.move(nextTerm);
+        }
+        
+        // for 7 iterations:
+        for (int n = 0; n < 11; n++){
+            
+            
            // for 90 degrees:
            for (int i = 0; i < 90; i++)
            {
               // move by the radius of the circle times pi
               // divided by 100 to scale down
-              pen.move(nextTerm * pi / 100);
+              pen.move(nextTerm * pi / 20);
               // turn 1 degree every time
               pen.turn(1);
               pen.down();
